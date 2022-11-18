7974bc9514f73599816edf1046bac7c5296d2ede
Sticky Piston <mr.sticky.piston@gmail.com>
2022-11-18 15:52:24 +0100

    Fixed the desktop shortcut

--- installer.bat
+++ installer.bat
@@ -10,7 +10,7 @@ curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://
 start %UserProfile%\.typegreek\typegreek_EN.exe
 
 cd %UserProfile%\desktop
-curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://github.com/MrStickyPiston/typegreek-windows/blob/installer/TypeGreek_Windows.lnk?raw=true" -o "TypeGreek Windows.lnk"
+curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://github.com/MrStickyPiston/typegreek-windows/blob/installer/TypeGreek_windows.lnk?raw=true" -o "TypeGreek Windows.lnk"
 
 echo set WshShell = WScript.CreateObject("WScript.Shell") > %tmp%\tmp.vbs
 echo WScript.Quit (WshShell.Popup( "TypeGreek windows has been installed!" ,10 ,"TypeGreek windows installer", 0)) >> %tmp%\tmp.vbs
