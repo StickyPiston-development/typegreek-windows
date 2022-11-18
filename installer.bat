40b8319364dc46c609200cf226334c4821a8b644
Sticky Piston <mr.sticky.piston@gmail.com>
2022-11-18 15:46:55 +0100

    Added desktop shortcut

--- installer.bat
+++ installer.bat
@@ -5,12 +5,15 @@ echo
 if not exist "%UserProfile%\.typegreek" mkdir %UserProfile%\.typegreek
 cd %UserProfile%\.typegreek
 
-curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://github.com/MrStickyPiston/typegreek-windows/blob/installer/TypeGreek_EN.exe" -o typegreek_EN.exe
-curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://github.com/MrStickyPiston/typegreek-windows/blob/installer/TypeGreek_NL.exe" -o typegreek_NL.exe
+curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://github.com/MrStickyPiston/typegreek-windows/blob/installer/TypeGreek_EN.exe?raw=true" -o typegreek_EN.exe
+curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://github.com/MrStickyPiston/typegreek-windows/blob/installer/TypeGrieks_NL.exe?raw=true" -o typegreek_NL.exe
 start %UserProfile%\.typegreek\typegreek_EN.exe
 
+cd %UserProfile%\desktop
+curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L "https://github.com/MrStickyPiston/typegreek-windows/blob/installer/TypeGreek_Windows.lnk?raw=true" -o "TypeGreek Windows.lnk"
+
 echo set WshShell = WScript.CreateObject("WScript.Shell") > %tmp%\tmp.vbs
-echo WScript.Quit (WshShell.Popup( "TypeGreek windows has been installed" ,10 ,"TypeGreek windows installer", 0)) >> %tmp%\tmp.vbs
+echo WScript.Quit (WshShell.Popup( "TypeGreek windows has been installed!" ,10 ,"TypeGreek windows installer", 0)) >> %tmp%\tmp.vbs
 cscript /nologo %tmp%\tmp.vbs
 del %tmp%\tmp.vbs
 
